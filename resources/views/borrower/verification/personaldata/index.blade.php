4fde117723d51b4c1b3b4caf24feea3c8b3fd1f5
destroylord <masapin68@gmail.com>
2021-08-17 11:56:41 +0700

    fix: Fixed validation file size max 2MB

--- resources/views/borrower/verification/personaldata/index.blade.php
+++ resources/views/borrower/verification/personaldata/index.blade.php
@@ -658,5 +658,24 @@
             alert("Hanya file jpg/jpeg dan png yang diperbolehkan!");
         }   
     }
+
+    // validation file size 2 MB
+    $('#scan_ktp').on('change', function(){
+        var numb = $(this)[0].files[0].size / 1024 /1024;
+        numb = numb.toFixed(2);
+        if (numb > 2) {
+            alert('Ukuran file maksimal 2MB. FIle anda berukuran: ' + numb + ' MB');
+            event.target.value = '';
+        }
+    });
+
+    $('#selfie_ktp').on('change', function(){
+        var numb = $(this)[0].files[0].size / 1024 /1024;
+        numb = numb.toFixed(2);
+        if (numb > 2) {
+            alert('Ukuran file maksimal 2MB. FIle anda berukuran: ' + numb + ' MB');
+            event.target.value = '';
+        }
+    });
 </script>
 @endpush
